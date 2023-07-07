dfe4107aa8c6fcc0246b78e51eeeb364b6c6ad02
nikita zm <zmanovskiy05@gmail.com>
2023-07-07 16:35:11 +0300

    32132

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,4 @@
 <script lang="ts">
-
- 
-	 
-	// import { v4 as uuid } from "uuid"
-
-	// const { $socket } = useNuxtApp()
-	// const uid = uuid()
- 
- 	// export default {
-	// 	mounted() {
-	// 		//@ts-ignore
-	// 		$socket.onopen = () => {
-			
-	// 			localStorage.setItem(`connection-${uid}`, uid)
-	// 		//@ts-ignore
-
-	// 			$socket.send(uid)
-	// 		}
-	// 		//@ts-ignore
-
-	// 		$socket.onclose = function () {
-	// 			console.log("disconnected")
-	// 		}
-	// 	}
-	// }
-
-
 </script>
 
 <template> 
